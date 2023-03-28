81a36104dbfcb75ddb8701f639bd1ccc7d65f6d0
Denis <den9055902096@gmail.com>
2023-03-28 22:52:24 +0300

    Merge remote-tracking branch 'main/main'

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import { React, useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
-import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
-import Header from './components/Header';
-import Calendar from './components/Calendar/Calendar';
-import Events from './components/Events/Events';
-import EventPage from './components/EventPage/EventPage';
-import RedirectPage from './components/RedirectPage';
-import defaultTheme from '@chakra-ui/theme';
-import { RecoilRoot } from 'recoil';
-const theme = extendBaseTheme({
-  components: {
-    Modal: defaultTheme.components.Modal,
-  },
-});
-
-export const url =
-  'https://run.mocky.io/v3/49b8fbae-13e6-4aac-a8d1-644e3881cc62';
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('https://run.mocky.io/v3/49b8fbae-13e6-4aac-a8d1-644e3881cc62')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
   return (
-    <RecoilRoot>
-      <ChakraBaseProvider theme={theme}>
-        <Header />
-        <Routes>
-          <Route path="/calendar" element={<Calendar initialData={data} />} />
-          <Route path="/events" element={<Events data={data} />} />
-          <Route path="/events/:id" element={<EventPage data={data} />} />
-          <Route path="*" element={<RedirectPage path="/calendar" />} />
-        </Routes>
-      </ChakraBaseProvider>
-    </RecoilRoot>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+            </Text>
+            <Link
+              color="teal.500"
+              href="https://chakra-ui.com"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Chakra
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
 }
+
 export default App;
