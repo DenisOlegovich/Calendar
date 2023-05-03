c441c06e8cf7a5bc8574e98b4164bcc3f9a0cde2
DenisOlegovich <den9055902096@gmail.com>
2023-05-03 18:05:13 +0300

    12345

--- src/components/EventPage/EventPage.js
+++ src/components/EventPage/EventPage.js
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Box, SimpleGrid, Spacer } from '@chakra-ui/react';
 import EventPageItem from './EventPageItem';
 import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
@@ -19,11 +19,11 @@ function EventPage() {
     },
     enabled: !!id,
   });
-
+  const [isSmallerThan1285] = useMediaQuery('(max-width: 1285px)');
   const todoItems = useRecoilValue(todoItemsState);
   return (
     <Box paddingBottom="20px" fontSize="14px">
-      <SimpleGrid spacingX="40px" spacingY="20px">
+      <Flex justifyContent={isSmallerThan1285 ? 'center' : 'space-between'}>
         {event && (
           <EventPageItem
             id={event.id}
@@ -36,7 +36,7 @@ function EventPage() {
             visitors={todoItems?.[id]?.visitors}
           />
         )}
-      </SimpleGrid>
+      </Flex>
     </Box>
   );
 }
