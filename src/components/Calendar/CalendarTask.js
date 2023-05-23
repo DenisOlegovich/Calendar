4a7a9e9fa2449be657096f77832bdbabb21cf8c2
Denis <den9055902096@gmail.com>
2023-05-23 22:47:03 +0300

    12345

--- src/components/Calendar/CalendarTask.js
+++ src/components/Calendar/CalendarTask.js
@@ -27,48 +27,6 @@ function CalendarTask({ title, date, description, image, id, deleteEvent }) {
   const [isSmallerThan600] = useMediaQuery('(max-width: 600px)');
 
   const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-  // function DeleteModal() {
-  //   return (
-  //     <Modal isOpen={isOpen} onClose={onClose}>
-  //       <ModalOverlay />
-  //       <ModalContent>
-  //         <ModalBody>
-  //           <Flex paddingTop="15px" alignItems="center" fontWeight="700">
-  //             <ExclamationCircle />
-  //             Вы уверены, что отказаться?
-  //           </Flex>
-  //         </ModalBody>
-
-  //         <ModalFooter>
-  //           <Button
-  //             colorScheme="blue"
-  //             border="1px solid"
-  //             fontSize="14px"
-  //             color="rgba(66, 66, 66, 0.45)"
-  //             fontWeight="700"
-  //             borderRadius="50px"
-  //             padding="7px 20px"
-  //             onClick={onClose}
-  //             marginRight="10px"
-  //           >
-  //             Отмена
-  //           </Button>
-  //           <Button
-  //             borderRadius="25px"
-  //             padding="7px 20px"
-  //             fontSize="14px"
-  //             color="white"
-  //             fontWeight="700"
-  //             bg="#1890FF"
-  //             onClick={() => deleteEvent(id)}
-  //           >
-  //             Ок
-  //           </Button>
-  //         </ModalFooter>
-  //       </ModalContent>
-  //     </Modal>
-  //   );
-  // }
   return (
     <Flex
       maxW="1500px"
@@ -121,7 +79,44 @@ function CalendarTask({ title, date, description, image, id, deleteEvent }) {
           </Link>
         </Flex>
       )}
-      {/* <DeleteModal /> */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <Flex paddingTop="15px" alignItems="center" fontWeight="700">
+              <ExclamationCircle />
+              Вы уверены, что отказаться?
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              border="1px solid"
+              fontSize="14px"
+              color="rgba(66, 66, 66, 0.45)"
+              fontWeight="700"
+              borderRadius="50px"
+              padding="7px 20px"
+              onClick={onClose}
+              marginRight="10px"
+            >
+              Отмена
+            </Button>
+            <Button
+              borderRadius="25px"
+              padding="7px 20px"
+              fontSize="14px"
+              color="white"
+              fontWeight="700"
+              bg="#1890FF"
+              onClick={() => deleteEvent(id)}
+            >
+              Ок
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
